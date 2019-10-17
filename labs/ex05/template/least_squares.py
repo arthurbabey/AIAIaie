d0091222e43946706a2617c041be39d32a8236fb
Arthur Babey <arthurbabey@tsf-476-wpa-5-125.epfl.ch>
2019-10-17 12:55:33 +0200

    ajout fichier pour ex05

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -4,14 +4,15 @@
 Least Square
 """
 
-import numpy as np
-
-
+from costs import *
+from grid_search import *
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """calculate the least squares solution."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
+    a = np.matmul(np.transpose(tx),tx)
+    b = np.matmul(np.transpose(tx),y)
+    w = np.linalg.solve(a,b)
+    MSE = compute_loss(y, tx, w)
+    return MSE, w
     # ***************************************************
     raise NotImplementedError
