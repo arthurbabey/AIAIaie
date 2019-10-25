fb0b3c278ce163d71eea0ea3ecfdcbb0e976da2c
bentoCH <50118862+bentoCH@users.noreply.github.com>
2019-10-25 17:05:31 +0200

    Add files via upload
    function to change -1 into 0 in y added

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math as m
 
+def change_negativeOnes_into_Zeros_in_y(y):
+    y[y == -1] = 0        
+    return y
 
 ### remove features with too many missing values
 def remove_features_with_too_many_missing_values(tX,proportion):
@@ -17,8 +20,8 @@ def remove_features_with_too_many_missing_values(tX,proportion):
 
     delete_ind = delete_ind.astype(int)
     data_removed_features = np.delete(data_removed_features,delete_ind, axis =1)
-    print(len(delete_ind), 'features removed','(features number: ', delete_ind, ')')
-    print('new shape of data:',data_removed_features.shape)
+    #print(len(delete_ind), 'features removed','(features number: ', delete_ind, ')')
+    #print('new shape of data:',data_removed_features.shape)
     return data_removed_features 
 
 #cool = remove_features_with_too_many_missing_values(tX,0.66)
@@ -27,7 +30,7 @@ def remove_features_with_too_many_missing_values(tX,proportion):
 def replace_missing_values_with_global_mean(tX):
     
     data_with_means_for_missing_values = np.copy(tX)
-    print(range(0,len(tX[0,:])))
+    #print(range(0,len(tX[0,:])))
     for feature_number in range(0,len(tX[0,:])):       
         #print(feature_number)
         mask = tX[:,feature_number] == -999.        
@@ -52,7 +55,6 @@ def Z_score_of_each_feature(tX):
         for sample_number in range(0,len(tX[:,0])):
             Standardized_data[sample_number,feature_number] = (Standardized_data[sample_number,feature_number] - mean_) / std_
     return Standardized_data
-
 
 
 
