19c32a01137a4751246a6657c558c5a7f2ec1be4
joeldaout <joel.daout@hotmail.ch>
2019-10-25 23:53:50 +0200

    better logistic regressions functions

--- scripts/logreg.py
+++ scripts/logreg.py
@@ -5,18 +5,14 @@ from helpers import batch_iter
 def sigmoid(t):
     """apply sigmoid function on t."""
     # ***************************************************
-   
     return np.exp(t) / (1 + np.exp(t))
-    
     # ***************************************************
     
     
-def calculate_lossreg(y, tx, w):
+def calculate_loss(y, tx, w):
     """compute the cost by negative log likelihood."""
     # ***************************************************    
-    
     return np.sum(np.log(1 + np.exp(np.matmul(tx, w))) - np.matmul(tx, w)*y)
-    
     # ***************************************************
 
     
@@ -24,61 +20,40 @@ def calculate_lossreg(y, tx, w):
 def calculate_gradient(y, tx, w):
     """compute the gradient of loss."""
     # ***************************************************
-    
     return np.matmul(np.transpose(tx), sigmoid(np.matmul(tx, w))-y)
-
     # ***************************************************
 
     
     
 def learning_by_gradient_descent(y, tx, w, gamma):
     """
-    Do one step of gradient descen using logistic regression.
+    Do one step of gradient descent using logistic regression.
     Return the loss and the updated w.
     """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # compute the cost: TODO
-    loss = calculate_lossreg(y, tx, w)
-
-    # ***************************************************
-    
+    loss = calculate_losslog(y, tx, w)
     gradient = calculate_gradient(y, tx, w)
-    # compute the gradient: TODO
 
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # update w: TODO
-    
     w = w - gamma*gradient
     
+    return loss, w
     # ***************************************************
 
-    return loss, w
 
-
-
-def logistic_regression_SGD(y, tx, initial_w, batch_size, gamma):
+def learning_by_log_regression(y, tx, w, batch_size, gamma):
     """Stochastic gradient descent algorithm."""
     # ***************************************************
-    w = initial_w
-    gradient=0
-    loss = 0
+    
     for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
         gradient = calculate_gradient(minibatch_y, minibatch_tx, w)
-        loss = calculate_lossreg(minibatch_y, minibatch_tx, w)
+        loss = calculate_loss(minibatch_y, minibatch_tx, w)
     gradient = gradient/batch_size
     loss = loss/batch_size
     
-
-    # ***************************************************
-    
     w = w-gamma*gradient
-    
-    # ***************************************************
-    
-    return loss, w
 
+    return loss, w
+    # ***************************************************
 
 
 
@@ -86,9 +61,6 @@ def logistic_regression_SGD(y, tx, initial_w, batch_size, gamma):
 def calculate_hessian(y, tx, w):
     """return the hessian of the loss function."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # calculate hessian: TODO
-    
     S = np.zeros((len(y), len(y)))
     
     for i in range(len(y)):
@@ -96,8 +68,6 @@ def calculate_hessian(y, tx, w):
         S[i,i] = a*(1 - a)
     
     return np.matmul(np.matmul(np.transpose(tx), S), tx)
-    #return np.matmul(np.matmul(np.transpose(tx), S), tx)
-    
     # ***************************************************
     
     
@@ -105,9 +75,6 @@ def calculate_hessian(y, tx, w):
 def logistic_regression(y, tx, w):
     """return the loss, gradient, and hessian."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # return loss, gradient, and hessian: TODO
-    
     loss = calculate_loss(y, tx, w)
     gradient = calculate_gradient(y, tx, w)
     hessian = calculate_hessian(y, tx, w)
@@ -118,59 +85,83 @@ def logistic_regression(y, tx, w):
     
     
     
-def learning_by_newton_method(y, tx, w):
+def learning_by_newton_method(y, tx, w, gamma):
     """
     Do one step on Newton's method.
     return the loss and updated w.
     """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # return loss, gradient and hessian: TODO
-    
     loss, gradient, hessian = logistic_regression(y, tx, w)
-
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # update w: TODO
     
-    w = w - np.matmul(np.linalg.inv(hessian),gradient)
-    # ***************************************************
+    w = w - np.matmul(gamma*np.linalg.inv(hessian),gradient)
+
     return loss, w
+    # ***************************************************
+    
 
 
-
-def penalized_logistic_regression(y, tx, w, lambda_):
+def penalized_logistic_regression(y, tx, w, lambda_,batch_size):
     """return the loss, gradient, and hessian."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # return loss, gradient, and hessian: TODO
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+        gradient = calculate_gradient(minibatch_y, minibatch_tx, w)+lambda_*w
+        loss = calculate_loss(minibatch_y, minibatch_tx, w)
+    gradient = gradient/batch_size
+    loss = loss/batch_size
     
-    gradient = calculate_gradient(y, tx, w)+lambda_*w
-    #hessian = calculate_hessian(y, tx, w)
-    loss = calculate_loss(y, tx, w)
-    
-    return loss, gradient#, hessian
-    
+    return loss, gradient
     # ***************************************************
 
 
-def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
+def learning_by_penalized_gradient(y, tx, w, gamma, lambda_,batch_size):
     """
     Do one step of gradient descent, using the penalized logistic regression.
     Return the loss and updated w.
     """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # return loss, gradient: TODO
-    
-    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_)
-    # ***************************************************
-    #raise NotImplementedError
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # update w: TODO
+    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_,batch_size)
     
     w = w - gamma*gradient
-    
-    # ***************************************************
+
     return loss, w
+    # ***************************************************
+
+
+    
+def running_gradient(y, tx, lambda_, method='log'):
+    """
+    run gradient descent, using logistic regression, 
+    penalized log regression or newton method.
+    Return the loss and final weights.
+    """
+    # ***************************************************
+    batch_size = 1000
+    max_iter = 10000
+    gamma = 0.01
+    threshold = 1e-8
+    losses = []
+    w = np.zeros((tx.shape[1], 1))
+
+    # start gradient descent
+    for iter in range(max_iter):
+        # get loss and update w.
+        if method == 'log':
+            loss, w = learning_by_log_regression(y, tx, w, batch_size, gamma)
+        if method == 'penalized':
+            loss, w = learning_by_penalized_gradient(y, tx, w, gamma, lambda_, batch_size)
+        if method == 'newton':
+            loss, w = learning_by_newton_method(y, tx, w, gamma)
+        # log info
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+            
+    return loss, w
+    # ***************************************************
+
+
+
+
