d1a994e33b6fa4439cba89a1eab6f9f7ce8f9d82
joeldaout <joel.daout@hotmail.ch>
2019-10-26 18:03:09 +0200

    ajouté implementations

---
+++ scripts/functions_for_implementations.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss with MSE.
+    """
+    # ***************************************************
+    return 1/(2*y.shape[0])*np.sum((y-np.matmul(tx, w))**2)
+    # ***************************************************
+    
+    
+    
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    # ***************************************************
+    error = y-np.matmul(tx,w)
+    return -1/len(y)*np.matmul(np.transpose(tx),error)
+    # ***************************************************
+    
+
+    
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    # ***************************************************
+    error = y-np.matmul(tx,w)
+    return -1/len(y)*np.matmul(np.transpose(tx),error)      
+    # ***************************************************
+    
+    
+    
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    # ***************************************************
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+    # ***************************************************            
+            
+
+
+def sigmoid(t):
+    """apply sigmoid function on t."""
+    # ***************************************************
+    return 1 / (1 + np.exp(-t))
+    # ***************************************************
+    
+    
+    
+def calculate_loss(y, tx, w):
+    """compute the cost by negative log likelihood."""
+    # ***************************************************    
+    return np.sum(np.log(1 + np.exp(np.matmul(tx, w))) - np.matmul(tx, w)*y)
+    # ***************************************************
+
+    
+    
+def calculate_gradient(y, tx, w):
+    """compute the gradient of loss."""
+    # ***************************************************
+    return np.matmul(np.transpose(tx), sigmoid(np.matmul(tx, w))-y)
+    # ***************************************************
+    
+
+    
+def learning_by_log_regression(y, tx, w, batch_size, gamma):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    #calculate gradient using minibatch
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+        gradient = calculate_gradient(minibatch_y, minibatch_tx, w)
+        loss = calculate_loss(minibatch_y, minibatch_tx, w)
+    gradient = gradient/batch_size
+    loss = loss/batch_size
+    
+    w = w-gamma*gradient
+
+    return loss, w
+    # ***************************************************
+    
+
+
+def penalized_logistic_regression(y, tx, w, lambda_,batch_size):
+    """return the loss, gradient"""
+    # ***************************************************
+    #calculate gradient using minibatch
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+        gradient = calculate_gradient(minibatch_y, minibatch_tx, w)+lambda_*w
+        loss = calculate_loss(minibatch_y, minibatch_tx, w)
+    gradient = gradient/batch_size
+    loss = loss/batch_size
+    
+    return loss, gradient
+    # ***************************************************
+
+    
+
+def learning_by_penalized_gradient(y, tx, w, gamma, lambda_,batch_size):
+    """
+    Do one step of gradient descent, using the penalized logistic regression.
+    Return the loss and updated w.
+    """
+    # ***************************************************
+    #calculate loss and gradient
+    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_,batch_size)
+    
+    #update the weights
+    w = w - gamma*gradient
+
+    return loss, w
+    # ***************************************************    
+    
+    
+    
+    
+    
+    
+    
+            
---
+++ scripts/implementations.py
@@ -0,0 +1,133 @@
+import numpy as np
+from functions_for_implementations import *
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    # ***************************************************
+    # Define parameter loss
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        #compute gradient and loss
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_mse(y, tx, w)
+        
+        #update weights
+        w = w-gamma*gradient
+
+        # store loss
+        losses.append(loss)
+        
+        #converge criterion
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < 1e-8:
+            break
+    return w, losses[-1]
+    # ***************************************************
+
+    
+    
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    # ***************************************************
+    batch_size = 1
+    # Define parameter to store loss
+    losses = []
+    for n_iter in range(max_iters):
+        #calculate gradient using minibatch
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+            gradient = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
+        gradient = gradient/batch_size
+        loss = compute_mse(y, tx, w)
+
+        w = w-gamma*gradient
+
+        # store w and loss
+        losses.append(loss)
+        #converge criterion
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < 1e-8:
+            break
+            
+    return w, losses[-1]
+    # ***************************************************
+    
+    
+
+def least_squares(y, tx):
+    """calculate the least squares."""
+    # ***************************************************
+    #compute optimal weights
+    a = np.matmul(np.transpose(tx),tx)
+    b = np.matmul(np.transpose(tx),y)
+    w = np.linalg.solve(a,b)
+    
+    #compute loss
+    loss = compute_mse(y, tx, w)
+    return w, loss
+    # ***************************************************
+    
+    
+    
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    # ***************************************************
+    #compute optimal weights
+    a = np.matmul(np.transpose(tx),tx)+(2*len(y)*lambda_*np.eye(tx.shape[1]))
+    b = np.matmul(np.transpose(tx),y)
+    w = np.linalg.solve(a,b)
+    
+    #compute loss
+    loss = compute_mse(y, tx, w)
+    return w, loss
+    # ***************************************************
+    
+
+    
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """
+    run gradient descent, using logistic regression
+    Return the loss and final weights.
+    """
+    # ***************************************************
+    batch_size = 1
+    losses = []
+    w = initial_w
+
+    # start gradient descent
+    for iter in range(max_iter):
+        # get loss and update w using logistic regression.
+        loss, w = learning_by_log_regression(y, tx, w, batch_size, gamma)
+
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < 1e-8:
+            break
+            
+    return w, losses[-1] 
+    # ***************************************************    
+    
+    
+    
+def reg_logistic_regression(y, tx, lambda_ , initial_w, max_iters, gamma):
+    """
+    run gradient descent, using logistic regression
+    Return the loss and final weights.
+    """
+    # ***************************************************
+    batch_size = 1
+    losses = []
+    w = initial_w
+
+    # start gradient descent
+    for iter in range(max_iter):
+        # get loss and update w using regularized logistic regression.
+        loss, w = learning_by_penalized_gradient(y, tx, w, lambda_, batch_size)
+
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < 1e-8:
+            break
+            
+    return w, losses[-1] 
+    # *************************************************** 
+    
